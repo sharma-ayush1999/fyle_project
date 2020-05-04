b9ec01d28f40522a0d5376006a064ef189bc91f7
Ayush Sharma <sharma.ayush3103@gmail.com>
2020-05-04 22:31:49 +0530

    commited

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-right = document.getElementsByClassName('fa-check');
+ let right = document.getElementsByClassName('fa-check');
 for (let i = 0; i < right.length; i++) {
     right[i].style.color="#1DC49F";
 }
-cross = document.getElementsByClassName('fa-times');
+let cross = document.getElementsByClassName('fa-times');
 for (let i = 0; i < cross.length; i++) {
     cross[i].style.color="#FF3F57";
 }
